a6abdfb0a645e078372469550f7580a9c951edf3
Saga Vallin <moonbeam0035@gmail.com>
2022-02-18 13:47:59 +0100

    done

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Description
+
+A simple to-do list to add, mark complete and remove tasks.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import styles from "./components/styles.module.css";
+import Main from "./components/Main";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const time = Math.floor(Date.now() / 1000);
+
+  function createTask(name) {
+    setTasks((prev) => {
+      return [...prev, { name: name, id: time }];
+    });
+  }
+
+  function removeTask(id) {
+    const taskList = [...tasks].filter((task) => task.id !== id);
+    setTasks(taskList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className={styles.header}>
+        <div className={styles.background}></div>
+        <h1 className={styles.h1}>To Do</h1>
       </header>
+      <Main
+        className={styles.main}
+        tasks={tasks}
+        createTask={createTask}
+        removeTask={removeTask}
+      ></Main>
     </div>
   );
 }
